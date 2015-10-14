81c281b0182adad0120a3ea67d5b21cfb8cd926e
Marc G Chevrette <chevrm@gmail.com>
2015-10-14 15:21:49 -0500

    added threads var

--- mcpipe.pl
+++ mcpipe.pl
@@ -6,6 +6,8 @@ use Cwd 'abs_path';
 
 ## usage:  perl mcpipe.pl FWD.fq REV.fq prefix
 
+my $threads = 38;
+
 ## Setup script path
 my $script_dir = abs_path($0);
 $script_dir =~ s/\/mcpipe\.pl//;
@@ -17,7 +19,7 @@ my ($fwd, $rev, $pref) = (shift, shift, shift);
 system("perl $script_dir/ilmnartifactfa.pl $fwd $rev > tmpartifacts.fa");
 
 ## Musket
-system("musket -p 38 -inorder -omulti $pref.musket -k 31 536870912 $fwd $rev");
+system("musket -p $threads -inorder -omulti $pref.musket -k 31 536870912 $fwd $rev");
 system("mv $pref.musket.0 $pref.musket.0.fastq");
 system("mv $pref.musket.1 $pref.musket.1.fastq");
 
@@ -25,10 +27,10 @@ system("mv $pref.musket.1 $pref.musket.1.fastq");
 system("perl $script_dir/trimmomatic.pl $pref.musket.0.fastq $pref.musket.1.fastq tmpartifacts.fa");
 
 ## Flash pairs
-system("flash -t 38 -o $pref.tp $pref.musket.0.trimpair.fastq $pref.musket.1.trimpair.fastq");
+system("flash -t $threads -o $pref.tp $pref.musket.0.trimpair.fastq $pref.musket.1.trimpair.fastq");
 
 ## Ray
-system("mpiexec -c 38 Ray -k 31 -o $pref-ray-31 " .
+system("mpiexec -c $threads Ray -k 31 -o $pref-ray-31 " .
 	"-disable-scaffolder -disable-network-test " .
 	"-s $pref.tp.extendedFrags.fastq -p $pref.tp.notCombined_1.fastq " .
 	"$pref.tp.notCombined_2.fastq -s $pref.musket.0.trimunpair.fastq " .
